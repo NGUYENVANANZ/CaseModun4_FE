dcc20497512abdda83bce7c1e27208c292014fbb
Nguyễn Văn An <vanankv123@gmail.com>
2022-12-09 00:25:23 +0700

    an

--- js/an/home.js
+++ js/an/home.js
@@ -3,10 +3,14 @@ function accountImg() {
         type: "Post",
         url: "http://localhost:8081/home/" + token,
         success: function (data) {
-            let str = data;
-            document.getElementById("account").src = str;
-            document.getElementById("account1").src = str;
-            document.getElementById("account2").src = str;
+            let str = data.img;
+            let std = data.fullName;
+            document.getElementById("imgAccount").src = str;
+            document.getElementById("imgAccount1").src = str;
+            document.getElementById("imgAccount2").src = str;
+            document.getElementById("nameAccount").innerHTML = std;
+            document.getElementById("nameAccount1").innerHTML = std;
+
 
 
         },
@@ -17,3 +21,55 @@ function accountImg() {
 }
 
 accountImg();
+
+function showPage() {
+    $.ajax({
+        type: "Get",
+        url: "http://localhost:8081/page/" + token,
+        success: function (data) {
+            let str = "";
+            for (let i = 0; i < data.length; i++){
+str += `
+    <div class="status-field-container write-post-container">
+        <div class="user-profile-box">
+            <div class="user-profile">
+                <img src="${data[i].account.img}" alt="">
+                <div>
+                    <p>${data[i].account.fullName}</p>
+                    <small>${data[i].time}</small>
+                </div>
+            </div>
+            <div>
+                <a href="#"><i class="fas fa-ellipsis-v"></i></a>
+            </div>
+        </div>
+        <div class="status-field">
+            <p>${data[i].text} </p>
+            <img src="${data[i].img}" alt="">
+
+        </div>
+        <div class="post-reaction">
+            <div class="activity-icons">
+                <div><img src="images/like-blue.png" alt="">120</div>
+                <div><img src="images/comments.png" alt="">52</div>
+                <div><img src="images/share.png" alt="">35</div>
+            </div>
+            <div class="post-profile-picture">
+                <img src="images/profile-pic.png " alt=""> <i class=" fas fa-caret-down"></i>
+            </div>
+        </div>
+    </div>
+
+`
+                document.getElementById("page").innerHTML = str;
+            }
+
+
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+}
+
+showPage();
