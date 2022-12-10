de4c0d922032ade1d785c4fdd8a24928955d71e0
Nguyễn Văn An <vanankv123@gmail.com>
2022-12-10 12:30:36 +0700

    add

--- js/dinh/Like.js
+++ js/dinh/Like.js
@@ -1,22 +1,25 @@
-function like(id, i, number){
+function like(id, i){
 $.ajax ({
     type: "Post",
     url: "http://localhost:8081/page/" + id,
     headers: {"Authorization": "Bearer " + localStorage.getItem('token')},
     success: function (data){
+        let id = i + "p"
         if (data.like){
             document.getElementById(i).src ="images/like-blue.png";
-            document.getElementById(data.page.id).innerHTML = data.page.likePages.length;
+            document.getElementById(id).innerHTML = data.page.likePages.length;
         }else {
             document.getElementById(i).src ="images/like.png";
-            document.getElementById(data.page.id).innerHTML = data.page.likePages.length;
+            document.getElementById(id).innerHTML = data.page.likePages.length;
         }
+
     },
     error: function (error) {
         console.log(error);
     }
 })
 }
+
 
 
 
